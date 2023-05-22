d6f7f9113e4d271784f643c4b050f28a16a6e907
Elli Beres <elliftheria@proton.me>
2023-05-22 12:22:43 -0500

    Split up backprop and weight/bias updating into 2 functions

--- src/PerceptronNetwork.jl
+++ src/PerceptronNetwork.jl
@@ -22,19 +22,24 @@ function feed_forward(inputs, weights, biases)
     return a, z
 end
 
-function propogate_back(y, a, z, weights, biases, eta)
-  nablaC = a[end] .- y
+function propogate_back(y, a, z, weights)
   layers = length(weights)
+  nablaC = a[end] .- y
   delta = Vector{AbstractArray}(undef, layers)
   delta[layers] = nablaC .* drelu(z[layers])
-  new_biases = copy(biases)
-  new_weights = copy(weights)
-  new_biases[layers] = biases[layers] - delta[layers] .* eta
-  new_weights[layers] = weights[layers] - (delta[layers] * transpose(a[layers])) .* eta
   for l = layers-1:-1:1
     delta[l] = (transpose(weights[l+1]) * delta[l+1]) .* drelu(z[l])
+  end
+  return delta
+end
+
+function update_weights_and_biases(a, weights, biases, delta, eta)
+  layers = length(weights)
+  new_biases = copy(biases)
+  new_weights = copy(weights)
+  for l = layers:-1:1
     new_biases[l] = biases[l] .- delta[l] .* eta
-    new_weights[l] = weights[l] - (delta[l] * transpose(a[l])) .* eta
+    new_weights[l] = weights[l] .- (delta[l] * transpose(a[l])) .* eta
   end
   return new_weights, new_biases
 end
@@ -52,10 +57,12 @@ function train_network(inputs, y, layer_shapes, eta, iterations, print_frequency
     biases[layer] = Matrix(rand(layer_shapes[layer][1], 1))
   end
   a, z = feed_forward(inputs, weights, biases)
-  weights, biases = propogate_back(y, a, z, weights, biases, eta)
+  delta = propogate_back(y, a, z, weights)
+  weights, biases = update_weights_and_biases(a, weights, biases, delta, eta)
   for i = 2:iterations
     a, z = feed_forward(inputs, weights, biases)
-    weights, biases = propogate_back(y, a, z, weights, biases, eta)
+    delta = propogate_back(y, a, z, weights)
+    weights, biases = update_weights_and_biases(a, weights, biases, delta, eta)
     if i%print_frequency == 0
       println("iteration $(i): error = $(loss(y, a[end]))")
     end
@@ -84,10 +91,6 @@ a0 = [2.0; 1.0; 3.0]
 weights = [W1, W2, W3]
 
 biases = [b1, b2, b3]
-
-a, z = feed_forward(a0, weights, biases)
-
-n_weights, n_biases = propogate_back([0.8], a, z, weights, biases, 0.01)
 
 a, z, n_weights, n_biases = train_network(a0, [0.8], [(2, 3), (2, 2), (1, 2)], 0.01, 1e5, 1e4)
 
